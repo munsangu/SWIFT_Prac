8e9347e585e1740cd298b1d4a349baef00239ae2
munsangu <ds-3632@hanmail.net>
2022-10-25 14:08:20 +0900

    20221025

---
+++ loopPrac1.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+// 구구단 2단 ~ 9단 출력
+for i in 2...9 {
+    print("\(i)단")
+    for j in 1...9 {
+        print("\(i) * \(j) = \(i * j)")
+    }
+    print("=============")
+}
+
+print()
+
+
+// 3의 배수 찾기
+for z in 1...100 {
+    if(z % 3 == 0) {
+        print("3의 배수 발견: \(z)")
+        continue
+    }
+}
+
+print()
+
+// 이모지 반복
+for a in 1...5 {
+    for b in 0...4 {
+        if(a > b) {
+            print("😁",terminator: "")
+        }
+        continue
+    }
+    print()
+}

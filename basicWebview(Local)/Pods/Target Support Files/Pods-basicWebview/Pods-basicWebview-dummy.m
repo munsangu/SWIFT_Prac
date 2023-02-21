02082aa6b006555691db605228d69b47f26ff16b
munsangu <ds-3632@hanmail.net>
2023-02-21 23:14:42 +0900

    230221 webview local test

---
+++ basicWebview(Local)/Pods/Target Support Files/Pods-basicWebview/Pods-basicWebview-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_basicWebview : NSObject
+@end
+@implementation PodsDummy_Pods_basicWebview
+@end
---
+++ basicWebview(Local)/basicWebview/ViewController.swift
@@ -0,0 +1,62 @@
+// Resource가 Local File인 경우의 source
+
+import UIKit
+import WebKit
+import Alamofire
+
+class ViewController: UIViewController, WKNavigationDelegate {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(webView)
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            webView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor)
+        ])
+        
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html") {
+            webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+        }
+
+        let contentController = self.webView.configuration.userContentController
+        contentController.add(self, name: "toggleMessageHandler")
+        let js = """
+            var _selector = document.querySelector('input[name=myCheckbox]');
+            _selector.addEventListener('change', function(event) {
+                var message = (_selector.checked) ? "Toggle Switch is on" : "Toggle Switch is off";
+                if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.toggleMessageHandler) {
+                    window.webkit.messageHandlers.toggleMessageHandler.postMessage({
+                        "message": message
+                    });
+                }
+            });
+        """
+        let script = WKUserScript(source: js, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+        contentController.addUserScript(script)
+    }
+    
+    private lazy var webView: WKWebView = {
+        let webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        return webView
+    }()
+}
+
+extension ViewController:WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        guard let dict = message.body as? [String: AnyObject] else { return }
+        print(dict)
+        guard let message = dict["message"] else { return }
+        let script = "document.getElementById('value').innerText = \"\(message)\""
+        webView.evaluateJavaScript(script) { (result, error) in
+            if let result = result {
+                print("Label is updated with message: \(result)")
+            } else if let error = error {
+                print("An error occurred \(error)")
+            }
+        }
+    }
+}

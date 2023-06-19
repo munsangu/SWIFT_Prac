dfc8c857f329c854c00e6b031d239b8460dd1c80
munsangu <ds-3632@hanmail.net>
2023-06-19 14:54:21 +0900

    230619 slotmachine

---
+++ SlotMachine/SlotMachine/ViewController.swift
@@ -0,0 +1,158 @@
+//
+//  ViewController.swift
+//  SlotMachine
+//
+//  Created by 문상우 on 2023/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var emojiPickerView: UIPickerView!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var resultLable: UILabel!
+    
+    var imageArray = [String]()
+    var dataArray1 = [Int]()
+    var dataArray2 = [Int]()
+    var dataArray3 = [Int]()
+    var amazingFlag = false
+    var bounds: CGRect = CGRect.zero
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        bounds = startButton.bounds
+        imageArray = ["👻","👸","💩","😘","🍔","🤖","🍟","🐼","🚖","🐷"]
+        
+        for _ in 0...100 {
+            self.dataArray1.append((Int)(arc4random() % 10 ))
+            self.dataArray2.append((Int)(arc4random() % 10 ))
+            self.dataArray3.append((Int)(arc4random() % 10 ))
+        }
+        
+        resultLable.text = ""
+        
+        emojiPickerView.delegate = self
+        emojiPickerView.dataSource = self
+        
+        startButton.layer.cornerRadius = 6
+        startButton.layer.masksToBounds = true
+        
+    }
+    
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        startButton.alpha = 0
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.3, options: .curveEaseOut, animations: {
+            
+            self.startButton.alpha = 1
+            
+        }, completion: nil)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func startButtonTap(_ sender: UIButton) {
+        let index1: Int
+        let index2: Int
+        let index3: Int
+        if amazingFlag {
+            index1 = Int(arc4random()) % 90 + 3
+            index2 = dataArray2.firstIndex(of: dataArray1[index1])!
+            index3 = dataArray3.lastIndex(of: dataArray1[index1])!
+            
+        } else {
+            index1 = Int(arc4random()) % 90 + 3
+            index2 = Int(arc4random()) % 90 + 3
+            index3 = Int(arc4random()) % 90 + 3
+        }
+        
+        emojiPickerView.selectRow(index1, inComponent: 0, animated: true)
+        emojiPickerView.selectRow(index2, inComponent: 1, animated: true)
+        emojiPickerView.selectRow(index3, inComponent: 2, animated: true)
+        
+        
+        if(dataArray1[emojiPickerView.selectedRow(inComponent: 0)] == dataArray2[emojiPickerView.selectedRow(inComponent: 1)] && dataArray2[emojiPickerView.selectedRow(inComponent: 1)] == dataArray3[emojiPickerView.selectedRow(inComponent: 2)]) {
+            
+            resultLable.text = "Bingo!"
+            
+        } else {
+            
+            resultLable.text = "💔"
+            
+        }
+        
+        
+        //animate
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.1, initialSpringVelocity: 5, options: .curveLinear, animations: {
+            
+            self.startButton.bounds = CGRect(x: self.bounds.origin.x, y: self.bounds.origin.y, width: self.bounds.size.width - 20, height: self.bounds.size.height)
+            
+        }, completion: { (compelete: Bool) in
+            
+            UIView.animate(withDuration: 0.1, delay: 0.0, options: UIView.AnimationOptions(), animations: {
+                
+                self.startButton.bounds = CGRect(x: self.bounds.origin.x, y: self.bounds.origin.y, width: self.bounds.size.width, height: self.bounds.size.height)
+                
+            }, completion: nil)
+            
+        })
+    }
+    
+}
+
+extension ViewController : UIPickerViewDelegate, UIPickerViewDataSource {
+    // MARK:UIPickerViewDataSource
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 100
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    // MARK:UIPickerViewDelegate
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return 100.0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 100.0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        
+        let pickerLabel = UILabel()
+        
+        if component == 0 {
+            pickerLabel.text = imageArray[(Int)(dataArray1[row])]
+        } else if component == 1 {
+            pickerLabel.text = imageArray[(Int)(dataArray2[row])]
+        } else {
+            pickerLabel.text = imageArray[(Int)(dataArray3[row])]
+        }
+        
+        pickerLabel.font = UIFont(name: "Apple Color Emoji", size: 80)
+        pickerLabel.textAlignment = NSTextAlignment.center
+        
+        return pickerLabel
+    }
+}

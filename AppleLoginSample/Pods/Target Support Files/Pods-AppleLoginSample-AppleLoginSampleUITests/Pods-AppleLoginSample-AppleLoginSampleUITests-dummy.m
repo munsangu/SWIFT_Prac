f0b06e5436f6a30c2a650a3709394beeab2ea07a
munsangu <ds-3632@hanmail.net>
2023-06-26 11:11:19 +0900

    230626

---
+++ AppleLoginSample/Pods/Target Support Files/Pods-AppleLoginSample-AppleLoginSampleUITests/Pods-AppleLoginSample-AppleLoginSampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppleLoginSample_AppleLoginSampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AppleLoginSample_AppleLoginSampleUITests
+@end
---
+++ AppleLoginSample/Pods/Target Support Files/Pods-AppleLoginSample/Pods-AppleLoginSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppleLoginSample : NSObject
+@end
+@implementation PodsDummy_Pods_AppleLoginSample
+@end

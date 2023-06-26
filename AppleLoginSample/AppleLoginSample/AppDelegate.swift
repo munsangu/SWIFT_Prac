bb267af765726208d717ab0a0672b8fcc3bd64e8
munsangu <ds-3632@hanmail.net>
2023-06-26 16:21:56 +0900

    230626

--- AppleLoginSample/AppleLoginSample/AppDelegate.swift
+++ AppleLoginSample/AppleLoginSample/AppDelegate.swift
@@ -5,53 +5,29 @@
 //  Created by 문상우 on 2023/06/26.
 //
 
-import AuthenticationServices
 import UIKit
+import AuthenticationServices
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        self.window = window
-        
         let appleIDProvider = ASAuthorizationAppleIDProvider()
-        appleIDProvider.getCredentialState(forUserID: "UserID") { ( credentialState, error) in
+        appleIDProvider.getCredentialState(forUserID: KeychainItem.currentUserIdentifier) { (credentialState, error) in
             switch credentialState {
             case .authorized:
-                print("Authorized")
+                break
+            case .revoked, .notFound:
                 DispatchQueue.main.async {
-                    let signValidViewController = SignValidViewControll()
-                    window.rootViewController = signValidViewController
-                    window.makeKeyAndVisible()
+                    self.window?.rootViewController?.showLoginViewController()
                 }
-            case .revoked:
-                print("Revoked")
-            case .notFound:
-                print("Not found")
             default:
                 break
             }
         }
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+    
 }
-
